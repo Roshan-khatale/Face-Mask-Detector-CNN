f7521e7b4f9296eec68b0ff6075a9ae0282b6289
Roshan Khatale <rvkhatale@gmail.com>
2023-08-05 15:00:00 +0530

    Add real-time detection script

--- scripts/detect_mask.py
+++ scripts/detect_mask.py
@@ -1,13 +1,9 @@
-# This would run real-time mask detection with OpenCV
 import cv2
 
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
-    if not ret:
-        break
-    # Placeholder for detection logic
-    cv2.imshow('Frame', frame)
+    cv2.imshow("Mask Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
